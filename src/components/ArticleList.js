03ac9ea6b79097349ee02049200dfcd3194201cb
Betelihem <Betelihem>
2022-02-24 19:29:00 -0800

    commit tree b0530402ce280bd663b651bfefb3142688000e0c

---
+++ src/components/ArticleList.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Article from './Article'
+
+function ArticleList({posts}) {
+    // console.log(posts)
+    return (
+        <main>
+            {posts.map((posts) => {
+                return (
+                    <Article 
+                        key={posts.id}
+                        title={posts.title}
+                        date={posts.date}
+                        preview = {posts.preview}
+                    />
+                )
+            })}
+        </main>
+    )
+}
+export default ArticleList
